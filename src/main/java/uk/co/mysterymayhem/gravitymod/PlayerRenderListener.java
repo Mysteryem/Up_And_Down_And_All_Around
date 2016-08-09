09bf7f865b1aa92351bdf405b105bd98b89d811c
Thomas Barlow <github@mysterymayhem.co.uk>
2016-08-09 14:12:30 +0100

    Hotfix (3), attempt to fix incompatibility with other mods affecting player rendering

--- src/main/java/uk/co/mysterymayhem/gravitymod/PlayerRenderListener.java
+++ src/main/java/uk/co/mysterymayhem/gravitymod/PlayerRenderListener.java
@@ -1,9 +1,12 @@
 package uk.co.mysterymayhem.gravitymod;
 
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -16,44 +19,45 @@ public class PlayerRenderListener {
     private EntityPlayer player = null;
     private boolean rotationNeedsUndo = false;
 
-    //@SubscribeEvent
-    public void onRender(RenderPlayerEvent.Pre event) {
-        EntityPlayer entityPlayer = event.getEntityPlayer();
-        if (GravityMod.proxy.gravityManagerCommon.isPlayerUpsideDown(entityPlayer)) {
-            this.rotationNeedsUndo = true;
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void onRender(RenderLivingEvent.Pre<EntityPlayer> event) {
+        EntityLivingBase entity = event.getEntity();
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer entityPlayer = (EntityPlayer)entity;
+            if (GravityMod.proxy.gravityManagerCommon.isPlayerUpsideDown(entityPlayer)) {
+                this.rotationNeedsUndo = true;
 
-            GlStateManager.pushMatrix();
-            GlStateManager.translate(event.getX(), event.getY(), event.getZ());
-            GlStateManager.rotate(180, 1, 0, 0);
-            GlStateManager.rotate(180, 0, 1, 0);
-            GlStateManager.translate(0, -entityPlayer.height, 0);
-            GlStateManager.translate(-event.getX(), -event.getY(), -event.getZ());
+                GlStateManager.pushMatrix();
+                GlStateManager.translate(event.getX(), event.getY(), event.getZ());
+                GlStateManager.rotate(180, 1, 0, 0);
+                GlStateManager.rotate(180, 0, 1, 0);
+                GlStateManager.translate(0, -entityPlayer.height, 0);
+                GlStateManager.translate(-event.getX(), -event.getY(), -event.getZ());
 
-            this.player = entityPlayer;
-            player.rotationYawHead *= -1;
-            player.rotationYaw *= -1;
-            player.rotationPitch *= -1;
-            player.prevRotationPitch *= -1;
-            player.prevRotationYaw *= -1;
-            player.renderYawOffset *= -1;
-            player.prevRenderYawOffset *= -1;
-            player.prevRotationYawHead *= -1;
+                this.player = entityPlayer;
+                player.rotationYawHead *= -1;
+                player.rotationYaw *= -1;
+                player.rotationPitch *= -1;
+                player.prevRotationPitch *= -1;
+                player.prevRotationYaw *= -1;
+                player.renderYawOffset *= -1;
+                player.prevRenderYawOffset *= -1;
+                player.prevRotationYawHead *= -1;
+            }
+
+            //if (entityPlayer == Minecraft.getMinecraft().thePlayer)
+
+
+            //entityPlayer.cameraYaw += 180;
+            //entityPlayer.cameraPitch += 180;
+            //entityPlayer.prevCameraYaw += 180;
+            //entityPlayer.prevCameraPitch += 180;
         }
-
-        //if (entityPlayer == Minecraft.getMinecraft().thePlayer)
-
-
-
-
-        //entityPlayer.cameraYaw += 180;
-        //entityPlayer.cameraPitch += 180;
-        //entityPlayer.prevCameraYaw += 180;
-        //entityPlayer.prevCameraPitch += 180;
 
     }
 
-    //@SubscribeEvent
-    public void onRender(RenderPlayerEvent.Post event) {
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    public void onRender(RenderLivingEvent.Post<EntityPlayer> event) {
         if (this.rotationNeedsUndo) {
             this.rotationNeedsUndo = false;
             player.rotationYawHead *= -1;
