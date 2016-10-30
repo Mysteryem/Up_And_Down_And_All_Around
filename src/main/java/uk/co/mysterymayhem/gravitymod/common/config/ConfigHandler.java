7c40bb38927cbaf9a7b3f4de81c0fed607e2b431
Thomas Barlow <github@mysterymayhem.co.uk>
2016-10-30 14:19:19 +0000

    Moved mod compatibility config into its own file and modified the comments

--- src/main/java/uk/co/mysterymayhem/gravitymod/common/config/ConfigHandler.java
+++ src/main/java/uk/co/mysterymayhem/gravitymod/common/config/ConfigHandler.java
@@ -1,5 +1,7 @@
 package uk.co.mysterymayhem.gravitymod.common.config;
 
+import com.sun.org.apache.xml.internal.utils.StringToStringTable;
+import joptsimple.internal.Strings;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import uk.co.mysterymayhem.gravitymod.GravityMod;
@@ -9,8 +11,12 @@ import uk.co.mysterymayhem.gravitymod.common.util.prepostmodifier.CombinedPrePos
 import uk.co.mysterymayhem.gravitymod.common.util.prepostmodifier.EnumPrePostModifier;
 import uk.co.mysterymayhem.gravitymod.common.util.prepostmodifier.IPrePostModifier;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
- * Created by Thomas on 2016-10-23.
+ * Main config file class
+ * Created by Mysteryem on 2016-10-23.
  */
 public class ConfigHandler {
     public enum EnumItemStackUseCompat {
@@ -18,14 +24,17 @@ public class ConfigHandler {
                 "modCompat.onUseOnBlock",
 
                 "Adding an item to this list will apply the compatibility when the item is right clicked on a block.\n" +
-                "\tSpecifically when the item's \"onItemUse\" method is called.\n"
+                "\tSpecifically when the item's \"onItemUse\" method is called from within ItemStack::onItemUse.\n"
         ),
         GENERAL(
                 "modCompat.onUseGeneral",
 
                 "Adding an item to this list will apply the compatibility when the item is right clicked on air, or on\n" +
                         "\ta block that doesn't open a chest/machine GUI or otherwise change state, such as right clicking \n" +
-                        "\ton a vanilla lever.\n\nThe Tinkers' Construct Rapier makes the player jump upwards and backwards\n" +
+                        "\ton a vanilla lever. Specifically when the item's \"onItemRightClick\" method is called from\n" +
+                        "\twithin ItemStack::useItemRightClick.\n\n" +
+
+                        "The Tinkers' Construct Rapier makes the player jump upwards and backwards\n" +
                         "\tslightly when right clicked. We want the player to jump upwards relative to their view of the\n" +
                         "\tworld, so we will need at least use relative Y motion for this compatibility. We also want the\n" +
                         "\tplayer to jump backwards relative to their view of the world. The rapier uses the player's\n" +
@@ -52,40 +61,68 @@ public class ConfigHandler {
         }
     }
 
-    private static final String MAIN_COMMENT = "Up And Down changes how player motion is used to move the player. Some\n" +
-            "\titems may move the player in unexpected ways when used. If that's the case, try adding them to one of the\n" +
-            "\tbelow lists. The format is (text in square brackets is optional)\n\n" +
+    private static final String MAIN_COMMENT =
+            "Up And Down changes how player motion is used to move the player. Some items may move the player in\n" +
+                    "\tunexpected ways when used. If that's the case, try adding them to one of the below lists. The\n" +
+                    "format is (text in square brackets is optional)\n\n" +
 
-            "\t\"<mod id>:<item name>[:damage][:damage][...],<compatibility modifier>:[compatibility modifier]\"\n\n" +
+                    "\t\"<mod id>:<item name>[:damage][:damage][...],<compatibility modifier>:[compatibility modifier]\"\n\n" +
 
-            "Where a compatibility modifier is case insensitive and one of:\n" +
-            "relativeRotation, relativeMotionAll, relativeMotionX, relativeMotionY, relativeMotionZ, absoluteMotionX,\n" +
-            "\tabsoluteMotionY or absoluteMotionZ\n\n" +
+                    "Where a compatibility modifier is case insensitive and one of:\n" +
+                    "relativeRotation, relativeMotionAll, relativeMotionX, relativeMotionY, relativeMotionZ, absoluteMotionX,\n" +
+                    "\tabsoluteMotionY or absoluteMotionZ\n\n" +
 
-            "These modify the player's rotation and/or motion before the item is used (and then puts the rotation/motion\n" +
-            "\tback to normal, taking into account any changes to motion, _but not taking into account any changes to\n" +
-            "\trotation_)\n." +
-            "You may use a max of one type of modifier per entry (up to 1 motion modifier and up to 1 rotation modifier).\n" +
-            "The default state when using items is that both rotation and motion are absolute (as if the player currently\n" +
-            "\thas downwards gravity).\n" +
-            "Using relativeMotionX implies that Z and Y motion will be absolute.\n" +
-            "Likewise, using absoluteMotionY implies that X and Z motion will be relative.\n" +
-            "So if you wanted both X and Y motion to be relative, you would use absoluteMotionZ.\n\n" +
+                    "These modify the player's rotation and/or motion before the item is used (and then puts the rotation/motion\n" +
+                    "\tback to normal, taking into account any changes to motion, !!but not taking into account any changes to\n" +
+                    "\trotation!!).\n\n" +
 
-            "Examples:\n" +
-            "'minecraft:stick:0:4,relativeMotionX' - Adds a relative X motion modifier to vanilla sticks with damage\n" +
-            "\tvalues 0 and 4.\n" +
-            "'tconstruct:longsword,relativeMotionAll:relativeRotation' - Adds a relative  X, Y and Z motion modifier\n" +
-            "\tcombined with a relative rotation modifier to all damage values of Tinkers' Construct Longswords.";
+                    "You may use a max of one type of modifier per entry (up to 1 motion modifier and up to 1 rotation modifier).\n\n" +
 
-    private static Configuration forgeConfig;
+                    "The default state when using items is that both rotation and motion are absolute (as if the player currently\n" +
+                    "\thas downwards gravity).\n\n" +
+
+                    "Using relativeMotionX implies that Z and Y motion will be absolute.\n\n" +
+
+                    "Likewise, using absoluteMotionY implies that X and Z motion will be relative.\n\n" +
+
+                    "So if you wanted both X and Y motion to be relative, you would use absoluteMotionZ.\n\n" +
+
+                    "Examples:\n" +
+                    "'minecraft:stick:0:4,relativeMotionX' - Adds a relative X motion modifier to vanilla sticks with damage\n" +
+                    "\tvalues 0 and 4.\n" +
+                    "'tconstruct:longsword,relativeMotionAll:relativeRotation' - Adds a relative X, Y and Z motion modifier\n" +
+                    "\tcombined with a relative rotation modifier to all damage values of Tinkers' Construct Longswords.";
+
+    private static Configuration modCompatibilityConfig;
+    private static final String CONFIG_DIRECTORY_NAME = "UpAndDownAndAllAround";
+    private static final String MOD_COMPATIBILITY_CONFIG_FILE_NAME = "modCompat.cfg";
 
     public static void loadConfig(FMLPreInitializationEvent event) {
-        ConfigHandler.forgeConfig = new Configuration(event.getSuggestedConfigurationFile());
+        File modConfigurationDirectory = event.getModConfigurationDirectory().toPath().resolve(CONFIG_DIRECTORY_NAME).toFile();
+        if (!modConfigurationDirectory.exists()) {
+            if (!modConfigurationDirectory.mkdirs()) {
+                throw new RuntimeException(new IOException("Could not create config folder"));
+            }
+        }
+        if (!modConfigurationDirectory.isDirectory()) {
+            throw new RuntimeException(new IOException("Config folder already exists, but is a file and not a folder"));
+        }
+
+        File modCompatConfigFile = modConfigurationDirectory.toPath().resolve(MOD_COMPATIBILITY_CONFIG_FILE_NAME).toFile();
+        if (!modCompatConfigFile.exists()) {
+            try {
+                if (!modCompatConfigFile.createNewFile()) {
+                    throw new RuntimeException(new IOException("Unable to create mod compatibility config file"));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        ConfigHandler.modCompatibilityConfig = new Configuration(modCompatConfigFile);
     }
 
     public static void processConfig() {
-        Configuration config = ConfigHandler.forgeConfig;
+        Configuration config = ConfigHandler.modCompatibilityConfig;
         config.load();
 
         config.addCustomCategoryComment(Configuration.CATEGORY_GENERAL, MAIN_COMMENT);
@@ -100,7 +137,7 @@ public class ConfigHandler {
     }
 
     private static void processStringList(EnumItemStackUseCompat compatType) {
-        String[] stringList = ConfigHandler.forgeConfig.getStringList(
+        String[] stringList = ConfigHandler.modCompatibilityConfig.getStringList(
                 compatType.configString, Configuration.CATEGORY_GENERAL, compatType.defaults, compatType.comment);
 
         toNextEntry:
